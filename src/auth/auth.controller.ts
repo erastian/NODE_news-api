3e9fb54c06b25f372db5fd4bc1776573a2b39509
erastian <erastian@gmail.com>
2023-05-16 00:55:38 +0200

    Work branch
    Add enum with roles to scheme. Add refreshToken f()

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,13 +4,18 @@ import { BaseController } from '../common/base.controller';
 import { TYPES } from '../types';
 import { ILogger } from '../services/logger/logger.interface';
 import { ValidateMiddleware } from '../common/validate.middleware';
+import jwt from 'jsonwebtoken';
 import { IConfigService } from '../config/config.service.interface';
+import { StatusCodes } from 'http-status-codes';
+import { HTTPError } from '../services/errors/http-error.class';
 
 import { IAuthController } from './auth.controller.interface';
 import { IAuthService } from './auth.service.interface';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { UserLoginDto } from './dto/user-login.dto';
 import UserDTO from '../users/dto/user.dto';
+import { ITokenPayload } from './auth.service';
+import { IUsersService } from '../users/users.service.interface';
 
 @injectable()
 export class AuthController extends BaseController implements IAuthController {
@@ -18,6 +23,7 @@ export class AuthController extends BaseController implements IAuthController {
 		@inject(TYPES.ILogger) private loggerService: ILogger,
 		@inject(TYPES.IConfigService) private configService: IConfigService,
 		@inject(TYPES.IAuthService) private authService: IAuthService,
+		@inject(TYPES.IUsersService) private userService: IUsersService,
 	) {
 		super(loggerService);
 		this.bindRoutes([
@@ -34,7 +40,7 @@ export class AuthController extends BaseController implements IAuthController {
 				middlewares: [new ValidateMiddleware(UserLoginDto)],
 			},
 			{ path: '/logout', method: 'get', func: this.logoutUser },
-			{ path: '/token', method: 'get', func: this.getToken, middlewares: [] },
+			{ path: '/token', method: 'get', func: this.getToken },
 			{ path: '/forget-password', method: 'post', func: this.forgotPassword, middlewares: [] },
 			{ path: '/restore-password', method: 'post', func: this.restorePassword, middlewares: [] },
 			{ path: '/activate/:link', method: 'get', func: this.activate, middlewares: [] },
@@ -42,7 +48,29 @@ export class AuthController extends BaseController implements IAuthController {
 	}
 
 	async getToken(req: Request, res: Response, next: NextFunction): Promise<void> {
-		this.ok(res, {});
+		try {
+			const { refreshTokenFromRequest } = req.cookies;
+
+			if (!refreshTokenFromRequest) {
+				return next(new HTTPError(StatusCodes.FORBIDDEN, 'Access denied.'));
+			}
+
+			const userFromToken = jwt.verify(
+				refreshTokenFromRequest,
+				this.configService.get('JWT_REFRESH_SECRET'),
+			) as ITokenPayload;
+
+			const user = await this.userService.getUserByEmail(userFromToken.email);
+
+			const { token: accessToken } = this.authService.getAuthToken(user);
+			const { cookie: refreshToken } = this.authService.getRefreshToken(user);
+
+			res.setHeader('Set-Cookie', [refreshToken]);
+
+			this.ok(res, { accessToken });
+		} catch (e) {
+			return next(e);
+		}
 	}
 
 	async activate(req: Request, res: Response, next: NextFunction): Promise<void> {
